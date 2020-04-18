b33ded4142cafe2c8564ae6730ebb3caa759c78f
Michal <zienowit@gmail.com>
2020-04-18 15:07:52 +0200

    i

--- src/app/listofusers/listofusers.component.ts
+++ src/app/listofusers/listofusers.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { MatSort, MatTableDataSource } from '@angular/material';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-listofusers',
@@ -17,7 +18,7 @@ export class ListOfUsersComponent implements OnInit {
   constructor(private http: HttpClient) {}
 
   getMessageTableData() {
-    return this.http.get('http://localhost:4200/listofusers');
+    return this.http.get('http://localhost:8080/listofusers');
   }
 
   ngOnInit() {
