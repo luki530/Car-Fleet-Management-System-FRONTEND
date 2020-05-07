7b7edded4be3a1b1e4ffd2d16ee3195c997419c6
Łukasz Oszczypała <lukasz.oszczypala@gmail.com>
2020-05-07 12:28:59 +0200

    contact us

--- src/app/_services/connection.service.ts
+++ src/app/_services/connection.service.ts
@@ -2,15 +2,15 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-providedIn: 'root'
+  providedIn: 'root'
 })
 export class ConnectionService {
-url: string = 'http://localhost:3000/send';
-constructor(private http: HttpClient) { }
+  url: string = 'https://backend.carfleetmanagementsystem.pl/contactus';
+  constructor(private http: HttpClient) { }
 
-sendMessage(messageContent: any) {
-  return this.http.post(this.url,
-  JSON.stringify(messageContent),
-  { headers: new HttpHeaders({ 'Content-Type': 'application/json' }), responseType: 'text' });
-}
+  sendMessage(messageContent: any) {
+    return this.http.post(this.url,
+      JSON.stringify(messageContent),
+      { headers: new HttpHeaders({ 'Content-Type': 'application/json' }), responseType: 'text' });
+  }
 }
