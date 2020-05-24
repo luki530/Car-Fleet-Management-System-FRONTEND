22d4e8578ba9c0de9eb147fda1dadc78b6a2239d
Łukasz Oszczypała <lukasz.oszczypala@gmail.com>
2020-05-24 15:35:19 +0200

    picker fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,7 +19,36 @@ import { ThemeService } from "./theme.service";
 })
 export class AppComponent implements OnInit {
 
-  options$: Observable<Array<Option>> = this.themeService.getThemeOptions();
+  options = [
+    {
+      "backgroundColor": "#fff",
+      "buttonColor": "#ffc107",
+      "headingColor": "#673ab7",
+      "label": "Deep Purple & Amber",
+      "value": "deeppurple-amber"
+    },
+    {
+      "backgroundColor": "#fff",
+      "buttonColor": "#ff4081",
+      "headingColor": "#3f51b5",
+      "label": "Indigo & Pink",
+      "value": "indigo-pink"
+    },
+    {
+      "backgroundColor": "#303030",
+      "buttonColor": "#607d8b",
+      "headingColor": "#e91e63",
+      "label": "Pink & Blue Grey",
+      "value": "pink-bluegrey"
+    },
+    {
+      "backgroundColor": "#303030",
+      "buttonColor": "#4caf50",
+      "headingColor": "#9c27b0",
+      "label": "Purple & Green",
+      "value": "purple-green"
+    }
+  ];
 
   private roles: string[];
   isLoggedIn = false;
