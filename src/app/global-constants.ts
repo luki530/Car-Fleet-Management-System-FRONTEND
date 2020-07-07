5f02c2efd62fc0fc01fa30cc9e1c3be164b834dd
Łukasz Oszczypała <lukasz.oszczypala@gmail.com>
2020-07-07 22:04:02 +0200

    fix

--- src/app/global-constants.ts
+++ src/app/global-constants.ts
@@ -1,4 +1,4 @@
 export class GlobalConstants {
-  public static URL: string = "https://backend.carfleetmanagementsystem.pl:443/";
-  //public static URL: string = "http://localhost:8080/";
+  //public static URL: string = "https://backend.carfleetmanagementsystem.pl:443/";
+  public static URL: string = "http://localhost:8080/";
 }
